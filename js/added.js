9fd6788c8a28aff961aa459dfb234423733c823a
engineer-kufre <engineer_kufre@yahoo.com>
2019-10-11 20:30:11 +0100

    modified addedjs

--- js/added.js
+++ js/added.js
@@ -1,23 +1,7 @@
-// CKEditor
-
-ClassicEditor
-.create( document.querySelector( '#body' ), {
-    toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
-    heading: {
-        options: [
-            { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-            { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-            { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-        ]
-    }
-} )
-.catch( error => {
-    console.log( error );
-} );
-
-// User Signup
-
 $(document).ready(function() {
+
+  // User Signup
+
   $('.regSubmitBtn').click(function(event) {
     event.preventDefault();
     const fullname = $('#fullname').val();
@@ -44,11 +28,58 @@ $(document).ready(function() {
               password,
             },
             success: function() {
-              $('#signup').trigger('reset');
+              window.location.assign('userlogin.html');
             },
           });
         }
       },
     });
   });
+
+  // User Signin
+
+  $('.signinbutton').click(function(event) {
+      event.preventDefault();
+      const usernameSI = $('#usernameSI').val();
+      const passwordSI = $('#passwordSI').val();
+      $.ajax({
+          method: 'GET',
+          url: `http://localhost:3000/users?username=${usernameSI}&password=${passwordSI}`,
+          data: {
+              username: usernameSI,
+              password: passwordSI,
+          },
+          success: function(response) {
+              if (response.length) {
+                  localStorage.setItem('username', usernameSI);
+                  window.location.assign('index.html');
+              } else {
+                  alert('Username or Password Incorrect');
+              }
+          },
+      });
+  });
+
+  // User Logout
+
+  $('.userlogout').click(function() {
+    localStorage.clear();
+  });
+
+  // CKEditor
+
+  ClassicEditor
+  .create( document.querySelector( '#body' ), {
+      toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+      heading: {
+          options: [
+              { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+              { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+              { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+          ]
+      }
+  } )
+  .catch( error => {
+      console.log( error );
+  } );
 });
