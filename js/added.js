f724486ca972911bee932464472223df6e78b9fa
engineer-kufre <engineer_kufre@yahoo.com>
2019-10-14 01:38:47 +0100

    updated addedjs

--- js/added.js
+++ js/added.js
@@ -154,7 +154,6 @@ $(document).ready(function() {
         $link.append(newPost.link);
         $body.append('<p>'+newPost.body+'</p>');
         $('#postform').trigger('reset');
-        alert('Article Posted'); 
         location.reload();       
       }
     });
@@ -236,6 +235,59 @@ $(document).ready(function() {
   $("#show").click(function() {
     $("form").fadeIn("slow").show();
   });
+
+ // POST Comment To DB
+
+  // var $usercomments = $('#usersname');
+  // $.ajax({
+  //   type: 'GET',
+  //   url: 'http://localhost:3000/users',
+  //   success: function(users){
+  //     if (users.length) {
+  //       $.each(users, function(i, user) {
+  //         $usercomments.append(`<li style="list-style-type:none;" data-id=${user.id}><p><span class="usernames">${user.username}</span></p>`);
+  //       });
+  //     } else {
+  //       $li.find('input.usercomment').val("Anonymous");
+  //     }
+  //   }
+  // });
+
+  var $username = $('#usernames');
+  var $comment = $('#comment');
+  $('#newCommentBtn').on('click', function(event){
+    event.preventDefault();
+    var comment = {
+      username: $username.val(),
+      comment: $comment.val(),
+    };
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3000/comment-post1',
+      data: comment,
+      success: function(newComment){
+        $username.append(newComment.username);
+        $comment.append(newComment.comment);
+        $('#comment-form').trigger('reset');
+        location.reload();       
+      }
+    });
+  });
+
+    // Display Comment
+
+  var $usercomments = $('#usercomment');
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:3000/comment-post1',
+    success: function(comments){
+      $.each(comments, function(i, comment) {
+        $usercomments.append(`<li style="list-style-type:none;" data-id=${comment.id}><p><span style="font-size: 20px; color: rgb(2, 2, 70); text-decoration: underline;">${comment.username}</span></p><p><span class="link" style="text-align: justify;">${comment.comment}</span></p>`);
+      });
+    }
+  });
+
+  
 
   // CKEditor
 
